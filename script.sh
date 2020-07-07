507763013236281eec93cce94d63b9916039a189
sara <saronah1099@gmail.com>
2020-07-07 14:18:39 +0300

    add -y

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install update 
+sudo apt install update -y
 sudo apt install docker.io -y &&/ docker-compose -y
 sudo apt install vim jq curl make 
 sudo usermod -aG docker $USER
