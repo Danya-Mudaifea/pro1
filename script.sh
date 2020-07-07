b6f2b7f812bafc8f0e49df7ee4a03e6f4128becd
sara <saronah1099@gmail.com>
2020-07-07 13:58:38 +0300

    editing script

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ sudo apt install update
 sudo apt install docker.io -y &&/ docker-compose -y
 sudo apt install vim jq curl make 
 sudo usermod -aG docker $USER
-chmod +x script.sh
+chmod 777 script.sh
