996dd1f32528a988b1e494e94f9f56d4c3b76125
sara <saronah1099@gmail.com>
2020-07-07 19:00:17 +0300

    add oh-my-bash

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 sudo apt install docker.io -y 
 sudo apt install docker-compose -y
-sudo apt install vim jq curl make -y 
+sudo apt install vim jq curl make oh-my-bash -y 
 sudo usermod -aG docker $USER
 chmod 777 script.sh
